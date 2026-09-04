8cf5d81c6fc9cf3e7f71cb068a1eb6128e5f430e
thanush1229 <thanushthota19@gmail.com>
2026-09-04 15:05:16 +0530

    arthematic operators.py

---
+++ #membership operator.py
@@ -0,0 +1,13 @@
+#membership operator 
+result= "a" in "apple"
+print("Result of a in apple is:",result)
+result= "a" not in "apple"
+print("Result of a is not in apple is:",result)
+
+
+
+a= [1,2,3]
+print("1 in a is:",1 in a)
+print("1 is not in a is:",1 not in a) 
+print("5 in a is:",5 in a)
+print("5 is not in a is:",5 not in a)
---
+++ arthematic operators.py
@@ -0,0 +1,23 @@
+
+a=2
+b=5
+
+print("Addition of a and b:,a + b")#addition operator
+
+#substraction operator
+print("Substraction of a and b :",a - b)
+
+#multipication operator
+print("multipication of a and b :",a * b)
+
+#Division operator
+print("Division of a and b :",a / b)
+
+#modulos operator
+print("modulos of a and b :",a % b)
+
+#floor division
+print("floor division of a and b :",a // b)
+
+#exponentiation operator
+print("exponentiation of a and b :",a ** b)
