7eba2d685f5f5eb04b6f6a6493d0e7bdfe244f41
thanush1229 <thanushthota19@gmail.com>
2026-09-04 14:38:25 +0530

    identity operators exercise.py

---
+++ #Identity operators exercise.py
@@ -0,0 +1,16 @@
+#Identity operators exercise
+a=[1,2,3]
+b=a
+result=b is a  #is identity operators
+Result=b is not a
+print("result of",b,"is",a,"is:",result)
+print("result of",b,"is not",a,"is",Result)
+
+
+c=[4,5,6]
+d=c
+reslut =d is c
+result=d is c  #is identity operators
+Result=d is not c
+print("result of",d,"is",c,"is:",result)
+print("result of",d,"is not",c,"is",Result)
