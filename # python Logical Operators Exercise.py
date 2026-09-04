6137c79c5043cad34aad6571175b9290b545a937
thanush1229 <thanushthota19@gmail.com>
2026-09-04 15:07:09 +0530

    # python logical operators exercise.py

---
+++ # python Logical Operators Exercise.py
@@ -0,0 +1,9 @@
+# python Logical Operators Exercise
+a= 1000
+result=a>500 and a<3000 #Logical operators
+print("result of",a,"> 500 and",a,"<30:",result,)
+
+a=5
+print(a<2 or a>2)
+result=a<2 and a>2
+print("result of",a,"<2 and",a,">2;result")
